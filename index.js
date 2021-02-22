3d230ad25fee24cf1160a6a41053513e0d368d2c
Erik Huang <erikhuangdrums@gmail.com>
2021-02-22 11:19:04 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+let dodger = document.querySelector('#dodger');
+let game = document.querySelector('#game');
+dodger.style.backgroundColor = "crimson";
+
+const moveDodgerLeft = () => {
+    let leftNums = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNums, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+const moveDodgerRight = () => {
+    let leftNums = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNums, 10);
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
